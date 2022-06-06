3fc4c0641d21ce7d05e77e1dfa7abb0f4f37e514
Kunj <shahkunj42@gmail.com>
2022-06-06 12:10:34 -0400

    commit tree 37e2a2b2801f0af4bd2249675a132391f38e25aa

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    let hq = 42;
+    return Math.abs(blockNumber - hq);
+}
+
+function distanceFromHqInFeet(blockNumberStart){
+    return 264 * distanceFromHqInBlocks(blockNumberStart);
+}
+
+function distanceTravelledInFeet(start, end){
+    return Math.abs(start - end) * 264;
+}
+function calculatesFarePrice(start, end){
+    let feet = distanceTravelledInFeet(start, end)
+    if (feet < 400){
+        return 0;
+    }
+    else if (feet < 2000){
+        return 0.02 * (feet - 400)
+    }
+    else if(feet < 2500){
+        return 25
+    }
+    else 
+    return 'cannot travel that far'
+}
